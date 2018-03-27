713bc33a9d1e8ed20ae9878404586a9e66671df2
khshahed <khshahed@users.noreply.github.com>
2018-03-27 17:03:26 +0600

    Server and Client working.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ server/config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    database: 'mongodb://admin:Enosis1234@ds119129.mlab.com:19129/amazon',
+    port: 3000
+}
